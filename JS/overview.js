f0eae36d13c2f09b854e563292eef28d165c21bc
Axelhrdz <axelhernandezperez23@gmail.com>
2024-01-14 17:55:33 -0600

    failed navbar in overeview test

---
+++ JS/overview.js
@@ -0,0 +1,7 @@
+// const menuToggle = document.querySelector('.toggle-overview')
+// const showcase = document.querySelector('main');
+
+// menuToggle.addEventListener('click', () => {
+//   menuToggle.classList.toggle('active');
+//   showcase.classList.toggle('active');
+// })
